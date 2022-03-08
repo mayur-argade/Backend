04be99d1d33783c0ba96358bf9dd5f0334e475ed
mayur-argade <argademayur2002@gmail.com>
2022-03-09 01:43:04 +0530

    register route done

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-require("dotenv").config()
-require('./config/database').connect()
+require("dotenv").config();
+require('./config/database').connect();
+const express = require("express");
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('./model/user')
 
-const bcrypt = require('bcryptjs')
-const express = require('express')
 const app = express();
-app.use(express.json)
 
-app.get("/", (req,res) => {
-    res.send("Authentication system")
-})
 
-app.post("/register", async(req,res) =>{
-    const { firstname, lastname, email, password } = req.body
+app.use(express.json());
 
-    if (!(email && password && firstname && lastname )) {
-        res.status(400).send("all fields are mandatory")
+app.get("/", (req, res) => {
+  res.send("Authentication system");
+});
+
+app.post("/register", async(req, res) => {
+    try {
+        const { firstname, lastname, email, password } = req.body; // get all the information
+    
+    if(!(email && password && firstname && lastname)){
+        res.status(400).send("all fields are required");
     }
 
     const existingUser = await User.findOne({ email });
@@ -24,14 +28,32 @@ app.post("/register", async(req,res) =>{
         res.status(401).send("user already exits");
     }
 
-    const myEncPassword = await bcrypt.hash(password, 10)
+    const myEncPass = await bcrypt.hash(password, 10);
 
     const user = await User.create({
         firstname,
         lastname,
-        email: email.toLowerCase,
-        password: myEncPassword
-    })
-})
+        email: email.toLowerCase(),
+        password: myEncPass,
+    });
 
-module.exports = app
+    // token creation
+    const token = jwt.sign(
+        {user_id: user._id, email},
+        process.env.SECRET_KEY,
+        {
+            expiresIn: '2h'
+        }
+    )
+    user.token = token
+
+    user.password = undefined
+    // update pr not
+    res.status(201).json(user)
+    } catch (error) {
+        console.log(error);  
+        
+    }
+});
+
+module.exports = app;
--- config/database.js
+++ config/database.js
@@ -4,14 +4,14 @@ const {MONGODB_URL} = process.env
 
 exports.connect = () => {
     mongoose.connect(MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+        useNewUrlParser: true, //
+        useUnifiedTopology: true //
     })
     .then(
         console.log("DB connected succesfully")
     )
     .catch(error =>{
-        console.log("connected to database");
+        console.log("Database connection failed");
         console.log(error);
         process.exit(1)
     })
